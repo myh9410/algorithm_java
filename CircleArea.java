f31e98c9fade101c6af02e34391d0f0ccb5adbff
myh9410 <mjskks94@naver.com>
2020-02-07 09:38:56 +0900

    Delete CircleArea.java

--- CircleArea.java
+++
@@ -1,9 +0,0 @@
-package com.java.first;
-
-public class CircleArea {
-	public static void main(String[] args) {
-		int r = 5;
-		double s = (double) (r*r*3.14);
-		System.out.printf("반지름이 5Cm인 원의 넓이는 %.1f cm입니다.",s);
-	}
-}
